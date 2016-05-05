471dc171ea6ce79daaf7f18166dc3829bf615b70
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-05-04 23:28:50 -0400

    begin creating face detector

--- Holocraft/HCCreateViewController.swift
+++ Holocraft/HCCreateViewController.swift
@@ -12,6 +12,12 @@ class HCCreateViewController: UIViewController {
     
     let takePhotoButton = UIButton(type: .Custom)
     let choosePhotoButton = UIButton(type: .Custom)
+    
+    let backgroundPhotoImageView = UIImageView()
+    let compositeImageView = UIImageView()
+    let takePhotoImageView = UIImageView()
+    
+    let takePhotoVC = HCTakePhotoViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,17 +33,35 @@ class HCCreateViewController: UIViewController {
             view.addSubview(btn)
         }
         
-        let views = ["take": takePhotoButton, "choose": choosePhotoButton]
+        let views = ["take": takePhotoButton, "choose": choosePhotoButton, "back": backgroundPhotoImageView, "comp": compositeImageView, "takePhoto": takePhotoImageView]
         NSLayoutConstraint.activateConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[take][choose(take)]|", options: [.AlignAllTop, .AlignAllBottom], metrics: nil, views: views))
         takePhotoButton.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
         
         takePhotoButton.addTarget(self, action: #selector(takePhotoBtnPressed), forControlEvents: .TouchUpInside)
         
+        for imageView in [backgroundPhotoImageView, compositeImageView, takePhotoImageView] {
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(imageView)
+        }
+        
+        backgroundPhotoImageView.image = UIImage(named: "photoStack")
+        takePhotoImageView.image = UIImage(named: "camera")
+        compositeImageView.image = UIImage(named: "editImage")
+        
+        compositeImageView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        NSLayoutConstraint.activateConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[back(100)][comp(100)][takePhoto(100)]", options: [.AlignAllTop, .AlignAllBottom], metrics: nil, views: views))
+        NSLayoutConstraint.activateConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[back(100)]-100-|", options: [], metrics: nil, views: views))
+        
     }
     
     func takePhotoBtnPressed() {
-        let takePhotoVC = HCTakePhotoViewController()
-        navigationController?.pushViewController(takePhotoVC, animated: true)
+        
+        presentViewController(takePhotoVC, animated: true, completion: nil)
+        takePhotoVC.takePictureButton.addTarget(self, action: #selector(picBtnPressed), forControlEvents: .TouchUpInside)
+    }
+    
+    func picBtnPressed() {
+//        takePhotoVC.gpuVideoCamera
     }
 
 }
