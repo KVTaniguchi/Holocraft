4cb059f965e034d6c6bc403a85bf7a7b36e91aed
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-05-07 01:12:01 -0400

    applied mask

--- Holocraft/HCTakePhotoViewController.swift
+++ Holocraft/HCTakePhotoViewController.swift
@@ -19,6 +19,14 @@ class HCTakePhotoViewController: UIViewController {
     
     let engine = CameraEngine()
     let testView = UIView()
+    
+    let topMask = UIView()
+    let leftMask = UIView()
+    let rightMask = UIView()
+    let bottomMask = UIView()
+    
+    var screenWidth: CGFloat = 0.0
+    var screenHeight: CGFloat = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +39,15 @@ class HCTakePhotoViewController: UIViewController {
         engine.metadataDetection = .Face
         engine.startSession()
         
+        screenWidth = UIScreen.mainScreen().bounds.width
+        screenHeight = UIScreen.mainScreen().bounds.height
+        
         view.backgroundColor = UIColor.whiteColor()
+        
+        [topMask, leftMask, rightMask, bottomMask].forEach { view in
+            view.backgroundColor = UIColor.blackColor()
+            cameraView.addSubview(view)
+        }
         
         takePictureButton.setTitle("Take Picture", forState: .Normal)
         takePictureButton.backgroundColor = UIColor.orangeColor()
@@ -56,12 +72,22 @@ class HCTakePhotoViewController: UIViewController {
         cameraView.addSubview(testView)
     }
     
-    func displayLayerDetection(frame: CGRect) {
+    func displayLayerDetection(hardFrame: CGRect) {
+        var frame = hardFrame
+        frame.origin.x -= 20
+        frame.size.width += 20
+        frame.origin.y -= 100
+        frame.size.height += 100
         dispatch_async(dispatch_get_main_queue()) {
             self.testView.alpha = 1.0
-            UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.BeginFromCurrentState, animations: { 
+            UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.BeginFromCurrentState, animations: {
                 self.testView.frame = frame
                 self.testView.alpha = 0
+                
+                self.topMask.frame = CGRectMake(0, 0, self.screenWidth, frame.origin.y)
+                self.bottomMask.frame = CGRectMake(0, CGRectGetMaxY(frame), self.screenWidth, self.screenHeight - CGRectGetMaxY(frame))
+                self.leftMask.frame = CGRectMake(0, frame.origin.y, frame.origin.x, frame.height)
+                self.rightMask.frame = CGRectMake(CGRectGetMaxX(frame), frame.origin.y, self.screenWidth - CGRectGetMaxX(frame), frame.height)
             }, completion: nil)
         }
     }
